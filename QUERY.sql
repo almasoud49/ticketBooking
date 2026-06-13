b84f6d626de74e1fa93400bbfd1adcf3a7bb4ed7
Abdullah Al Masoud <almasoud49@yahoo.com>
2026-06-14 01:21:21 +0600

    [DONE] Solution of Query-03

--- QUERY.sql
+++ QUERY.sql
@@ -83,3 +83,8 @@ where tournament_category = 'Champions League' and match_status = 'Available'
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 select user_id, full_name, email from users 
 where full_name like 'Tanvir%' or full_name ilike '%Haque%';
+
+--Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+select  booking_id,user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status from bookings  
+where payment_status is null;
